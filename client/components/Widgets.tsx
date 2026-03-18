bce15fb3fd84253e4e2588990391152ed5d2e28d
Anton Frehser <hey@tonyfresher.com>
2026-03-18 15:58:39 +0100

    Fix spacing

--- client/components/Widgets.tsx
+++ client/components/Widgets.tsx
@@ -3,10 +3,10 @@ import { SpaceName } from './SpaceName'
 export function Widgets() {
   return (
     <div className="flex h-full flex-col">
-      <header className="pb-6">
+      <header className="pb-4">
         <SpaceName />
       </header>
-      <div className="grid grid-cols-2 grid-rows-[136px_136px_auto] gap-6">
+      <div className="grid grid-cols-2 grid-rows-[136px_136px_auto] gap-4">
         <div className="bg-black/4 col-span-2 rounded-xl" />
         <div className="bg-black/4 rounded-xl" />
         <div className="bg-black/4 rounded-xl" />
