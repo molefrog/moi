72930ca5442cade8287ba6a756bf9d1a7139598e
Anton Frehser <hey@tonyfresher.com>
2026-03-20 14:24:01 +0100

    Fix animation

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -53,7 +53,7 @@ export function App() {
           <div
             className={cn(
               'sticky top-10 h-[calc(100vh-4rem)] shrink-0 self-start transition-all ease-in-out',
-              showSidebar ? 'w-[464px] opacity-100 duration-0' : 'w-0 opacity-0 duration-200'
+              showSidebar ? 'w-[464px] opacity-100 duration-200' : 'w-0 opacity-0 duration-200'
             )}
           >
             <div className="h-full w-[464px] pl-6 lg:pl-16">{chatPanel}</div>
